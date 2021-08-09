3c75312d7938c96a71c677073425d8e3f1d55966
btzr-io <btzr.io@gmail.com>
2021-08-09 00:19:24 -0500

    improve podcast detection

--- src/analysis/streams.py
+++ src/analysis/streams.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sync import sync_streams_data
 from utils import get_streams_cannonical_url
 from .tags import process_tags
+from constants import STREAM_TYPE
+from vocabulary import MULTILINGUAL
 
 
 def process_streams(df):
@@ -21,6 +23,14 @@ def process_streams(df):
     # Merge tags data
     df_streams = df_streams.drop(columns="tags")
     df_streams = pd.merge(df_streams, df_tags, on="claim_id")
+    # Classify untagged content:
+    # It is possible to detect podcast episodes by title.
+    df_streams.loc[
+        df_streams.stream_type.isnull()
+        & df_streams.title.str.contains("|".join(MULTILINGUAL["PODCAST"]), case=False),
+        "stream_type",
+    ] = STREAM_TYPE["PODCAST"]
     # Filter uknown types
     df_streams = df_streams.loc[df_streams["stream_type"].notnull()]
+
     return df_streams
