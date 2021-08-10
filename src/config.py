51fa761b19311a15d12f01d19d2712f55f6c1cce
btzr-io <btzr.io@gmail.com>
2021-08-09 21:29:58 -0500

    allow custom config: close #1

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
+import os
 from dotenv import dotenv_values
 
-config = dotenv_values(".env.default")
+config = {
+    # Load shared development variables
+    **dotenv_values(".env.default"),
+    # Load sensitive variables
+    **dotenv_values(".env"),
+    # Override loaded values with environment variables
+    **os.environ,
+}
